dc59ed5ea12be87161cb01a79b8216b863a0a46f
Michael Ullrich <michael.ullrich@nuvibit.com>
2024-02-29 11:22:08 +0100

    fix

--- test/complete_test.go
+++ test/complete_test.go
@@ -6,24 +6,24 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
-func TestSSO(t *testing.T) {
+func TestIdC(t *testing.T) {
 	// retryable errors in terraform testing.
-	t.Log("Starting SSO test")
+	t.Log("Starting ACF AWS IcD Module test")
 
-	terraformOptions := &terraform.Options{
+	terraformTest := &terraform.Options{
 		TerraformDir: "../examples/complete",
 		NoColor: false,
 		Lock: true,
 	}
 
-	defer terraform.Destroy(t, terraformOptions)
-	terraform.InitAndApply(t, terraformOptions)
+	defer terraform.Destroy(t, terraformTest)
+	terraform.InitAndApply(t, terraformTest)
 
-	testSuccess1Output := terraform.Output(t, terraformReadConfiguration, "test_success_1")
+	testSuccess1Output := terraform.Output(t, terraformTest, "test_success_1")
 
 	t.Log(testSuccess1Output)
 	// Assert that 'test_success_1' equals "true"
 	assert.Equal(t, "true", testSuccess1Output, "The test_success_1 output is not true")
 
-	terraform.Destroy(t, terraformOptions)
+	terraform.Destroy(t, terraformTest)
 }
