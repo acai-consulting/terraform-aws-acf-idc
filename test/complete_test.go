106895917320f7a9edf351f6fe2d3ab84e20e0ee
Michael Ullrich <michael.ullrich@nuvibit.com>
2024-04-08 10:12:43 +0200

    try

--- test/complete_test.go
+++ test/complete_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"log"
 	"testing"
 	"time"
 
@@ -9,7 +10,6 @@ import (
 )
 
 func TestIdC(t *testing.T) {
-	// retryable errors in terraform testing.
 	t.Log("Starting ACF AWS IcD Module test")
 
 	terraformIdC := &terraform.Options{
@@ -18,17 +18,16 @@ func TestIdC(t *testing.T) {
 		Lock:         true,
 	}
 
-	defer terraform.Destroy(t, terraformIdC)
-	terraform.InitAndApply(t, terraformIdC)
+	// Initialize and apply Terraform configuration
+	_, err := terraform.InitAndApplyE(t, terraformIdC)
+	if err != nil {
+		t.Fatalf("Failed to apply Terraform: %v", err)
+	}
 
 	testSuccess1Output := terraform.Output(t, terraformIdC, "test_success_1")
-	t.Log(testSuccess1Output)
-	// Assert that 'test_success_1' equals "true"
 	assert.Equal(t, "true", testSuccess1Output, "The test_success_1 output is not true")
 
 	testSuccess2Output := terraform.Output(t, terraformIdC, "test_success_2")
-	t.Log(testSuccess2Output)
-	// Assert that 'test_success_2' equals "true"
 	assert.Equal(t, "true", testSuccess2Output, "The test_success_2 output is not true")
 
 	terraformReporting := &terraform.Options{
@@ -37,21 +36,33 @@ func TestIdC(t *testing.T) {
 		Lock:         true,
 	}
 
-	defer terraform.Destroy(t, terraformReporting)
-	terraform.InitAndApply(t, terraformReporting)
+	_, err = terraform.InitAndApplyE(t, terraformReporting)
+	if err != nil {
+		t.Fatalf("Failed to apply Terraform: %v", err)
+	}
 
 	idcReportResult := terraform.OutputMap(t, terraformReporting, "idc_report_lambda_result")
-	t.Log(idcReportResult)
-
-	// Extract the statusCode and assert it
 	statusCode := idcReportResult["statusCode"]
-	// Print the status code
-	t.Logf("Derived StatusCode: %s", statusCode)
 	assert.Equal(t, "200", statusCode, "Expected statusCode to be 200")
 
-	// Explicitly destroy the IdC infrastructure - mitigate: error waiting for SSO Permission Set (arn:aws:sso:::permissionSet/ssoins-6987eff4f8663f8f/ps-3b5018dbc2c1002c) to provision: unexpected state 'FAILED', wanted target 'SUCCEEDED'.
-	terraform.Destroy(t, terraformIdC)
-	time.Sleep(10 * time.Second) // Wait for 10 seconds
-	terraform.Destroy(t, terraformIdC)
+	// Try to explicitly destroy the IdC infrastructure and log error if it fails
+	_, err = terraform.DestroyE(t, terraformIdC)
+	if err != nil {
+		log.Printf("Error during Terraform destroy: %v", err)
+	}
 
+	time.Sleep(10 * time.Second) // Wait for 10 seconds before trying again
+
+	// Attempt to destroy again
+	_, err = terraform.DestroyE(t, terraformIdC)
+	if err != nil {
+		log.Printf("Error during the second attempt of Terraform destroy: %v", err)
+	}
+
+	// Ensure reporting infrastructure is also destroyed at the end of the test
+	defer func() {
+		if _, err := terraform.DestroyE(t, terraformReporting); err != nil {
+			log.Printf("Error during Terraform destroy for reporting: %v", err)
+		}
+	}()
 }
